26f229e2c393dee75d9d8f347e926ec3bbe4e81b
loddrik <ruben.hermosilla@mail.udp.cl>
2021-05-15 16:57:25 -0400

    Lo puse mas bonito

--- trenes.cpp
+++ trenes.cpp
@@ -2,43 +2,43 @@
 using namespace std;
 int main()
 {
-	    int estacion[3][3];
+	int estacion[3][3];
 	        
-	        for(int i = 0; i< 3;i++){
-			        for(int j = 0; j< 3;j++){
+	for(int i = 0; i< 3;i++){
+		for(int j = 0; j< 3;j++){
 					            
-					            cin >> estacion[i][j];
-						                while(estacion[i][j] < 0){
-									                cin >> estacion[i][j];
-											            }
+			cin >> estacion[i][j];
+			while(estacion[i][j] < 0){
+				cin >> estacion[i][j];
+				}
 								            
-								        }    
-				    }
+		}    
+	}
 		    
-		    for(int i = 0; i<3; i++){
-			            int contador = 0;
-				            bool aux[3] ;
-					            for(int j = 0;j<3; j++){
-							                if(estacion[i][j] > 50){
-										                contador = contador + 1;
-												                aux[j] = true;
-														            }
+	for(int i = 0; i<3; i++){
+		int contador = 0;
+		bool aux[3] ;
+		for(int j = 0;j<3; j++){
+			if(estacion[i][j] > 50){
+				contador = contador + 1;
+				aux[j] = true;
+			}
 									            
-									        }
-						            if(contador >= 3){
-								                cout << "El tren numero " << i << " dejará de funcionar durante 1 día entero" << endl;
-										        }
-							            else{
-									                for(int k = 0; k<3; k++){
-												                if(aux[k] == true){
-															                    cout << "En el tren " << i<< " El vagón numero " << k << " Necesita una mantención rápida" << endl;
-																	                    }
-														            }
-											        }
-								        }
+		}
+		if(contador >= 3){
+			cout << "El tren numero " << i << " dejará de funcionar durante 1 día entero" << endl;
+		}
+		else{
+			for(int k = 0; k<3; k++){
+				if(aux[k] == true){
+					cout << "En el tren " << i<< " El vagón numero " << k << " Necesita una mantención rápida" << endl;
+				}
+			}
+		}
+	}
 
 
 		        
 		        
-		        return 0;
+	return 0;
 }
