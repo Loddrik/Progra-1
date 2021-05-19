98735848b9e137ee9684e0ee016c03ee57113e17
loddrik <ruben.hermosilla@mail.udp.cl>
2021-05-19 10:29:11 -0400

    Cambioes en variables M y N

--- trenes.cpp
+++ trenes.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 int main()
 {
-	int estacion[3][3];
+	int N, M;
+	cin >> N;
+	cin >> M;
+	int estacion[N][M];
 	        
-	for(int i = 0; i< 3;i++){
-		for(int j = 0; j< 3;j++){
+	for(int i = 0; i< N;i++){
+		for(int j = 0; j< M;j++){
 					            
 			cin >> estacion[i][j];
 			while(estacion[i][j] < 0){
@@ -15,21 +18,21 @@ int main()
 		}    
 	}
 		    
-	for(int i = 0; i<3; i++){
+	for(int i = 0; i<N; i++){
 		int contador = 0;
 		bool aux[3] ;
-		for(int j = 0;j<3; j++){
+		for(int j = 0;j<M; j++){
 			if(estacion[i][j] > 50){
 				contador = contador + 1;
 				aux[j] = true;
 			}
 									            
 		}
-		if(contador >= 3){
+		if(contador > N/2){
 			cout << "El tren numero " << i << " dejará de funcionar durante 1 día entero" << endl;
 		}
 		else{
-			for(int k = 0; k<3; k++){
+			for(int k = 0; k<M; k++){
 				if(aux[k] == true){
 					cout << "En el tren " << i<< " El vagón numero " << k << " Necesita una mantención rápida" << endl;
 				}
